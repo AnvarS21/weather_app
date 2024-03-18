203ce98a2a0cadd1ceabc99cf327e1ca23999829
AnvarS21 <shamsutdinovanvar21@gmail.com>
2024-03-18 15:44:51 +0600

    finish

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=30,
+                            unique=True)
+    class Meta:
+        verbose_name_plural = 'Города'
+        verbose_name = 'Город'
+
+    def __str__(self):
+       return f'{self.name}'
